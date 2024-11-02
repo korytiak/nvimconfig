827579529682ee765b1e900f6f38a8421fee7c17
Svetozar Korytiak <sveto@Svetozars-MacBook-Pro.local>
2024-11-02 15:11:30 +0100

    added luline

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,104 +1,70 @@
 return {
-        'neovim/nvim-lspconfig',
-        config = function()
-                require'lspconfig'.lua_ls.setup{}
-                -- Optional configuration for Harpoon
+        {
+                'VonHeikemen/lsp-zero.nvim',
+                config = function()
+                        vim.opt.signcolumn = 'yes'
 
-                require'lspconfig'.marksman.setup{}
-                require'lspconfig'.gopls.setup{}
-                require'lspconfig'.lemminx.setup{}
+                        -- Add cmp_nvim_lsp capabilities settings to lspconfig
+                        -- This should be executed before you configure any language server
+                        local lspconfig_defaults = require('lspconfig').util.default_config
 
-                -- luasnip setup
-                local luasnip = require 'luasnip'
+                        require('lspconfig').lua_ls.setup({})
+                        require'lspconfig'.marksman.setup{}
+                        require'lspconfig'.gopls.setup{}
+                        require'lspconfig'.lemminx.setup{}
+                        require'lspconfig'.ts_ls.setup{}
+                        require'lspconfig'.csharp_ls.setup{}
 
-                -- nvim-cmp setup
-                local cmp = require 'cmp'
-                cmp.setup {
-                        snippet = {
-                                expand = function(args)
-                                        luasnip.lsp_expand(args.body)
+                        lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+                        'force',
+                        lspconfig_defaults.capabilities,
+                        require('cmp_nvim_lsp').default_capabilities()
+                        )
+
+                        -- This is where you enable features that only work
+                        -- if there is a language server active in the file
+                        vim.api.nvim_create_autocmd('LspAttach', {
+                                desc = 'LSP actions',
+                                callback = function(event)
+                                        local opts = {buffer = event.buf}
+
+                                        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                                        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                                        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                                        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                                        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                                        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                                        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                                        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                                        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                                        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
                                 end,
-                        },
-                        mapping = cmp.mapping.preset.insert({
-                                ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-                                ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-                                ['<Tab>'] = cmp.mapping.confirm {
-                                        behavior = cmp.ConfirmBehavior.Replace,
-                                        select = true,
+                        })
+
+                        local cmp = require('cmp')
+
+                        cmp.setup({
+                                sources = {
+                                        {name = 'nvim_lsp'},
                                 },
-                        }),
-                        sources = {
-                                { name = 'nvim_lsp' },
-                                { name = 'luasnip' },
-                        },
-                }
-
-                -- init.lua or your Lua configuration file
-
-                -- Set up TypeScript language server
-                require('lspconfig').ts_ls.setup({
-                        on_attach = function(client, bufnr)
-                                -- Disable default formatting
-                                client.server_capabilities.document_formatting = false
-
-                                local function format_with_prettier()
-                                        local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
-                                        if ft == "typescript" or ft == "typescriptreact" then
-                                                vim.cmd([[silent! !prettier --write %]])
-                                                vim.cmd("edit!")
-                                        end
-                                end
-
-                                -- Format on save
-                                vim.api.nvim_create_autocmd("BufWritePre", {
-                                        buffer = bufnr,
-                                        callback = format_with_prettier,
-                                })
-                        end,
-                })
-
-                require'lspconfig'.omnisharp.setup {
-                        cmd = { "dotnet", "/Users/sveto/Projects/omnisharp/OmniSharp.dll" },
-                        filetypes = { "cs", "vb", "fs", "razor" },
-                        settings = {
-                                FormattingOptions = {
-                                        -- Enables support for reading code style, naming convention and analyzer
-                                        -- settings from .editorconfig.
-                                        EnableEditorConfigSupport = true,
-                                        -- Specifies whether 'using' directives should be grouped and sorted during
-                                        -- document formatting.
-                                        OrganizeImports = nil,
+                                snippet = {
+                                        expand = function(args)
+                                                -- You need Neovim v0.10 to use vim.snippet
+                                                vim.snippet.expand(args.body)
+                                        end,
                                 },
-                                MsBuild = {
-                                        -- If true, MSBuild project system will only load projects for files that
-                                        -- were opened in the editor. This setting is useful for big C# codebases
-                                        -- and allows for faster initialization of code navigation features only
-                                        -- for projects that are relevant to code that is being edited. With this
-                                        -- setting enabled OmniSharp may load fewer projects and may thus display
-                                        -- incomplete reference lists for symbols.
-                                        LoadProjectsOnDemand = nil,
-                                },
-                                RoslynExtensionsOptions = {
-                                        -- Enables support for roslyn analyzers, code fixes and rulesets.
-                                        EnableAnalyzersSupport = nil,
-                                        -- Enables support for showing unimported types and unimported extension
-                                        -- methods in completion lists. When committed, the appropriate using
-                                        -- directive will be added at the top of the current file. This option can
-                                        -- have a negative impact on initial completion responsiveness,
-                                        -- particularly for the first few completion sessions after opening a
-                                        -- solution.
-                                        EnableImportCompletion = nil,
-                                        -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
-                                        -- true
-                                        AnalyzeOpenDocumentsOnly = nil,
-                                },
-                                Sdk = {
-                                        -- Specifies whether to include preview versions of the .NET SDK when
-                                        -- determining which version to use for project loading.
-                                        IncludePrereleases = true,
-                                },
-                        },
-                }
-
-        end,
+                                mapping = cmp.mapping.preset.insert({}),
+                        })
+                end,
+        },
+        {'neovim/nvim-lspconfig',
+        dependencies = {
+                { 'williamboman/mason.nvim', config = true },
+                'williamboman/mason-lspconfig.nvim',
+                'WhoIsSethDaniel/mason-tool-installer.nvim',
+                { 'j-hui/fidget.nvim', opts = {} },
+                'hrsh7th/cmp-nvim-lsp',
+                'hrsh7th/nvim-cmp',
+        }
+        },
 }
